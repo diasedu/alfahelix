7baad7dd1e00fcc32f9e9ba79b495a3b80997c1c
eduardo <eduardodias830@gmail.com>
2023-07-15 09:39:00 -0300

    :tada: commit inicial

---
+++ application/views/template/scripts.php
@@ -0,0 +1,13 @@
+		<!-- Bootstrap core JavaScript
+			================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+		<script src="<?= base_url() ?>js/bootstrap.min.js"></script>
+		<script src="<?= base_url() ?>js/owl.carousel.min.js"></script>
+		<script src="<?= base_url() ?>js/cbpAnimatedHeader.js"></script>
+		<script src="<?= base_url() ?>js/theme-scripts.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="<?= base_url() ?>js/ie10-viewport-bug-workaround.js"></script>
+	</body>
+</html>
